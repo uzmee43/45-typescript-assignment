b3c35ffbcc0949117d73e99670e077473e4c6160
uzmee43 <uk0172634@gmail.com>
2024-04-23 08:12:02 -0700

    delete node_modules file

--- 3 Name Cases/app.ts
+++ 3 Name Cases/app.ts
@@ -1,9 +1,9 @@
-
+import { log } from "console";
 
 
 let persoName = "uzma khan";
 console.log(persoName.toLowerCase());
 console.log(persoName.toUpperCase());
-console.log(persoName.replace(/\b\w/g, (char) => char.toUpperCase()));
 
+console.log(persoName.replace(/\b\w/g, (char)=> char.toUpperCase()))
 
